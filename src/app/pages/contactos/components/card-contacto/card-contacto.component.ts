1289e53144f291da26b7bd4aedc66b28f76c1376
carlycode <ingcarlospaternina@gmail.com>
2021-01-05 08:40:50 -0500

    fix bug vercel deploy

--- src/app/pages/contactos/components/card-contacto/card-contacto.component.ts
+++ src/app/pages/contactos/components/card-contacto/card-contacto.component.ts
@@ -24,7 +24,7 @@ export class CardContactoComponent implements OnInit {
   @Output() contactoSeleccionado = new EventEmitter<IContacto>();
 
 
-  constructor(private router: Router, private _contacto: ContactoService) {}
+  constructor(private router: Router, public _contacto: ContactoService) {}
 
   ngOnInit(): void {}
 
